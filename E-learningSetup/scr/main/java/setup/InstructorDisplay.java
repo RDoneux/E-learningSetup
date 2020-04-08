153441b0b7f495995d7f00fb756040b9e2774569
R.Doneux <R.Doneux@LTC-PC-012.loddonschool.co.uk>
2020-04-08 22:27:10 +0100

    Copy started

--- E-learningSetup/scr/main/java/setup/InstructorDisplay.java
+++ E-learningSetup/scr/main/java/setup/InstructorDisplay.java
@@ -6,7 +6,16 @@ import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -54,10 +63,35 @@ public class InstructorDisplay extends JPanel implements MouseListener {
 				+ stringDate[2];
 
 		for (JLabel label : items) {
-			File fileLocation = new File(filePath + "\\" + label.getText());
+			File fileLocation = new File(filePath + "\\" + date + " " + type + "\\" + label.getText());
+			if (!fileLocation.exists()) {
+				fileLocation.mkdirs();
+
+				switch (type) {
+				case "Re-certification":
+					exportResource("presentation/Re-Certification - Presentation Feedback.docx", fileLocation,
+							label.getText() + " ~ Presentation Feedback.docx");
+					break;
+				case "Assessment Day":
+					break;
+				default:
+					break;
+				}
+
+			}
 			System.out.println(fileLocation.getPath());
 		}
 
+	}
+
+	private void exportResource(String resLocation, File destination, String docName) {
+		try {
+			Files.copy(Paths.get(this.getClass().getClassLoader().getResource(resLocation).toURI()),
+					Paths.get(destination.getAbsolutePath() + "//" + docName + ".docx"),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException | URISyntaxException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public ArrayList<JLabel> getItems() {
